728e11549ceed0129db43c008b625f57aecf558b
Matt Rendina <mrendina@stsci.edu>
2017-11-21 12:08:52 -0500

    Consolidate skipping code

--- vars/jfile_utils.groovy
+++ vars/jfile_utils.groovy
@@ -1,5 +1,30 @@
 // Jenkinsfile utilities
 
+def scm_checkout() {
+    skip_job = 0
+    node("on-master") {
+        stage("Setup") {
+            checkout(scm)
+            // Obtain the last commit message and examine it for skip directives.
+            logoutput = sh(script:"git log -1 --pretty=%B", returnStdout: true).trim()
+            if (logoutput.contains("[ci skip]") || logoutput.contains("[skip ci]")) {
+                skip_job = 1
+                currentBuild.result = 'SUCCESS'
+                println("\nBuild skipped due to commit message directive.\n")
+                return skip_job
+            }
+            stash includes: '**/*', name: 'source_tree'
+        }
+    }
+    return skip_job
+}
+
+//if (skip_job == 1) {
+//    currentBuild.result = 'SUCCESS'
+//    println("\nBuild skipped due to commit message directive.\n")
+//    return
+//}
+
 
 def concurrent2(mylist) {
     for (build in mylist) {
