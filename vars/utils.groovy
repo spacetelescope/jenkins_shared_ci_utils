cc6fd7e8f040b07f7fbc427d8a24530a05f76306
Matt Rendina <mrendina@stsci.edu>
2017-11-27 17:22:57 -0500

    Add some debug output

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -34,6 +34,10 @@ def concurrent2(configs) {
     println("Size of configs = ${configs.size()}")
     for (config in configs) {
         println("concurrent2: build.nodetype = ${config.nodetype}")
+        println("concurrent2: build.build_mode= ${config.build_mode}")
+        println("concurrent2: build.build_args= ${config.build_args}")
+        println("concurrent2: build.env_vars= ${config.env_vars}")
+
         tasks["${config.nodetype}/${config.build_mode}"] = {
             node(config.nodetype) {
                 //withEnv(config.env_vars) {
