84a4df525350bbda5be12fd713b25a9ec17a5320
Matt Rendina <mrendina@stsci.edu>
2017-11-29 12:30:12 -0500

    fix typo

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -45,7 +45,7 @@ def concurrent(configs) {
                 for (var in myconfig.env_vars) {
                     if (var.contains("PATH")) {
                         cwd = pwd()
-                        tvar = var.replace(".", cwd)env.PATH}"
+                        tvar = var.replace(".", cwd)
                         env.PATH = "${tvar}:${env.PATH}"
                     }
                 }
