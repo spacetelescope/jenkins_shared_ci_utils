960bcbf9e544c8670494368151a56c2ae6b10b83
Matt Rendina <mrendina@stsci.edu>
2017-11-30 13:09:53 -0500

    Remove cruft

--- src/BuildConfig.groovy
+++ src/BuildConfig.groovy
@@ -22,13 +22,13 @@ class BuildConfig implements Serializable {
     def skippedUnstableThresh= ''
 
     // Constructors
-    BuildConfig() {
-        nodetype = ""
-    }
+    //BuildConfig() {
+    //    nodetype = ""
+    //}
 
-    BuildConfig(nodetype) {
-        this.nodetype = nodetype
-    }
+    //BuildConfig(nodetype) {
+    //    this.nodetype = nodetype
+    //}
 
     // copy method requires Jenkins script approval for the
     // following signatures:
@@ -36,30 +36,10 @@ class BuildConfig implements Serializable {
     // method groovy.lang.MetaObjectProtocol getProperties
     // method groovy.lang.MetaProperty getProperty java.lang.Object
     // method groovy.lang.MetaProperty setProperty java.lang.Object java.lang.Object
-    def BuildConfig copy() {
-        BuildConfig.metaClass.getProperties().findAll(){it.getSetter()!=null}.inject(new BuildConfig()){
-            buildconfig,metaProp->metaProp.setProperty(buildconfig,metaProp.getProperty(this))
-            buildconfig
-        }
-    }
-}
-
-
-class BuildConfig2 implements Serializable {
-    def nodetype = ""
-    def build_mode = ""
-    def env_vars = []
-    def build_cmds = []
-    def test_cmds = []
-    def run_tests = true
-
-    def failedFailureNewThresh = ''
-    def failedFailureThresh = ''
-    def failedUnstableNewThresh = ''
-    def failedUnstableThresh= ''
-
-    def skippedFailureNewThresh = ''
-    def skippedFailureThresh = ''
-    def skippedUnstableNewThresh = ''
-    def skippedUnstableThresh= ''
+    //def BuildConfig copy() {
+    //    BuildConfig.metaClass.getProperties().findAll(){it.getSetter()!=null}.inject(new BuildConfig()){
+    //        buildconfig,metaProp->metaProp.setProperty(buildconfig,metaProp.getProperty(this))
+    //        buildconfig
+    //    }
+    //}
 }
