73378bef9b5d9b7aaef91d77a584a1c244ec9b64
Matt Rendina <mrendina@stsci.edu>
2017-11-30 11:10:30 -0500

    Clean out

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -101,24 +101,6 @@ def concurrent(configs) {
 } //end concurrent
 
 
-///**
-// * This method makes a "deep clone" of any object it is given.
-// */
-//def deepClone(object) {
-//    //try {
-//        ByteArrayOutputStream baos = new ByteArrayOutputStream()
-//        ObjectOutputStream oos = new ObjectOutputStream(baos)
-//        oos.writeObject(object)
-//        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray())
-//        ObjectInputStream ois = new ObjectInputStream(bais)
-//        return ois.readObject()
-//    //}
-//    //catch (Exception e) {
-//    //    e.printStackTrace()
-//    //    return null
-//    //}
-//}
-
 // standard deep copy implementation
 def deepcopy(orig) {
      bos = new ByteArrayOutputStream()
