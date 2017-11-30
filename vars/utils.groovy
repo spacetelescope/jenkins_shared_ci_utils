995622ebc7f2884747d3380641ed621641c5a77b
Matt Rendina <mrendina@stsci.edu>
2017-11-30 11:52:55 -0500

    Put it back

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -99,3 +99,14 @@ def concurrent(configs) {
         parallel(tasks)
     }
 } //end concurrent
+
+
+// standard deep copy implementation
+def deepcopy(orig) {
+     bos = new ByteArrayOutputStream()
+     oos = new ObjectOutputStream(bos)
+     oos.writeObject(orig); oos.flush()
+     bin = new ByteArrayInputStream(bos.toByteArray())
+     ois = new ObjectInputStream(bin)
+     return ois.readObject()
+}
