dab47d3726bf12b646a2c6c39a6ff1dc1200edf5
Matt Rendina <mrendina@stsci.edu>
2017-11-30 10:35:35 -0500

    Add new clone function

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -99,3 +99,22 @@ def concurrent(configs) {
         parallel(tasks)
     }
 } //end concurrent
+
+
+/**
+ * This method makes a "deep clone" of any object it is given.
+ */
+public static Object deepClone(Object object) {
+    try {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(object);
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        ObjectInputStream ois = new ObjectInputStream(bais);
+        return ois.readObject();
+    }
+    catch (Exception e) {
+        e.printStackTrace();
+        return null;
+    }
+}
