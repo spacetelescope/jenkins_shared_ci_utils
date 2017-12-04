1a655d972068ed45cd3e2b46ef09d1fa506a54cf
Matt Rendina <mrendina@stsci.edu>
2017-12-04 14:10:20 -0500

    Clean up class definition

--- src/BuildConfig.groovy
+++ src/BuildConfig.groovy
@@ -1,15 +1,12 @@
 // src/BuildConfig.groovy
 package BuildConfig;
 
-//@AutoClone  // annotation is not CPS-compatible?
 class BuildConfig implements Serializable {
     def nodetype = ""
     def build_mode = ""
     def env_vars = []
     def build_cmds = []
     def test_cmds = []
-    def run_tests = true
-    def env_vars_map = [:]
 
     def failedFailureNewThresh = ''
     def failedFailureThresh = ''
@@ -25,21 +22,4 @@ class BuildConfig implements Serializable {
     BuildConfig() {
         this.nodetype = ""
     }
-
-    //BuildConfig(nodetype) {
-    //    this.nodetype = nodetype
-    //}
-
-    // copy method requires Jenkins script approval for the
-    // following signatures:
-    // method groovy.lang.MetaBeanProperty getSetter
-    // method groovy.lang.MetaObjectProtocol getProperties
-    // method groovy.lang.MetaProperty getProperty java.lang.Object
-    // method groovy.lang.MetaProperty setProperty java.lang.Object java.lang.Object
-    //def BuildConfig copy() {
-    //    BuildConfig.metaClass.getProperties().findAll(){it.getSetter()!=null}.inject(new BuildConfig()){
-    //        buildconfig,metaProp->metaProp.setProperty(buildconfig,metaProp.getProperty(this))
-    //        buildconfig
-    //    }
-    //}
 }
