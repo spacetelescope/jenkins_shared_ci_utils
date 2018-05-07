263e289b6e01487649b45fdd32ebfe20a7af18de
Matt Rendina <mrendina@stsci.edu>
2018-05-07 12:20:06 -0400

    Initial commit

---
+++ src/BuildConfig.groovy
@@ -0,0 +1,26 @@
+// src/BuildConfig.groovy
+package BuildConfig;
+
+class BuildConfig implements Serializable {
+    def nodetype = ""
+    def build_mode = ""
+    def env_vars = []
+    def env_vars_raw = []
+    def build_cmds = []
+    def test_cmds = []
+
+    def failedFailureNewThresh = ''
+    def failedFailureThresh = ''
+    def failedUnstableNewThresh = ''
+    def failedUnstableThresh= ''
+
+    def skippedFailureNewThresh = ''
+    def skippedFailureThresh = ''
+    def skippedUnstableNewThresh = ''
+    def skippedUnstableThresh= ''
+
+    // Constructors
+    BuildConfig() {
+        this.nodetype = ""
+    }
+}
