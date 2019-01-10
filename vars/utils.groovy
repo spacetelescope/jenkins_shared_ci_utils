7fdf9a1688846233e0834acf299b966a2e6b952d
Matt Rendina <mrendina@stsci.edu>
2019-01-10 11:38:18 -0500

    github api import with implicit lib rather than @grab

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -8,7 +8,7 @@ import org.apache.commons.lang3.SerializationUtils
 import org.apache.commons.io.FilenameUtils
 
 //@Grab(group='org.kohsuke', module='github-api', version='1.93')
-//import org.kohsuke.github.GitHub
+import org.kohsuke.github.GitHub
 
 
 @NonCPS
