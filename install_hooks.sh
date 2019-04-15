f89fc195c5e005eec316fe8e7b6acb341f865622
Matt Rendina <mrendina@stsci.edu>
2019-04-15 12:12:37 -0400

    Fix hook installation

--- install_hooks.sh
+++ install_hooks.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Installing pre-commit git hook script to facilitate CI testing..."
-ln -s hooks/pre-commit .git/hooks/pre-commit
+ln -s ../../hooks/pre-commit .git/hooks/pre-commit
