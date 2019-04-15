01957741c8bb74cec6e72c734ba0b8589efa1f72
Matt Rendina <mrendina@stsci.edu>
2019-04-15 10:18:59 -0400

    Add tests dir; try out inclusion into jenkinsfile
    Cleanup jenkinsfile. Add hook instalation script
    
    Rename jenkinsfile.test
    
    Update hook to use renamed Jenkinsfile

---
+++ tests/test_25pass.py
@@ -0,0 +1,11 @@
+def test_25_1of4():
+    assert 1==1
+
+def test_25_2of4():
+    assert 1==2
+
+def test_25_3of4():
+    assert 1==3
+
+def test_25_4of4():
+    assert 1==4
---
+++ tests/test_alwayspass.py
@@ -0,0 +1,3 @@
+def test_alwayspass():
+    assert 1==1
+
