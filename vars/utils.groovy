b6bf5b3df051322515bd4827434fc29a5c435464
Matt Rendina <mrendina@stsci.edu>
2019-06-17 10:46:57 -0400

    Try dotglob mv to get all files

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -54,16 +54,16 @@ def scm_checkout(args = ['skip_disable':false]) {
             // Perform repo checkout, which for some reason clobbers everything
             // in the workspace. Then, create a project subdir, and move all
             // files into it. Then continue as usual.
-        checkout([$class: 'GitSCM', 
-            branches: [[name: '*/master']], 
-            doGenerateSubmoduleConfigurations: false, 
-            extensions: [[$class: 'RelativeTargetDirectory', 
-                relativeTargetDir: 'checkout-directory']], 
-            submoduleCfg: []]) //,
+        //checkout([$class: 'GitSCM', 
+        //    branches: [[name: '*/master']], 
+        //    doGenerateSubmoduleConfigurations: false, 
+        //    extensions: [[$class: 'RelativeTargetDirectory', 
+        //        relativeTargetDir: 'checkout-directory']], 
+        //    submoduleCfg: []]) //,
             //userRemoteConfigs: [[url: 'https://github.com/luxengine/math.git']]])
-            //checkout(scm)
-            //sh "mkdir clone"
-            //stat = sh(script: "mv * clone", returnStatus: true)
+            checkout(scm)
+            sh "mkdir clone"
+            stat = sh(script: "shopt -s dotglob; mv * clone", returnStatus: true)
             sh "ls -al"
             println("args['skip_disable'] = ${args['skip_disable']}")
             if (args['skip_disable'] == false) {
