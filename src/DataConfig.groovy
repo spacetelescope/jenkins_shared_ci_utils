6d0f2fb71ba24767e30b9adfebfd82fa88537086
Joseph Hunkeler <jhunkeler@gmail.com>
2019-10-02 13:30:15 -0400

    Add "managed" attribute

--- src/DataConfig.groovy
+++ src/DataConfig.groovy
@@ -5,6 +5,7 @@ class DataConfig implements Serializable {
     String root = '.'
     String server_id = ''
     String match_prefix = '(.*)'
+    Boolean managed = true
     Boolean keep_data = false
     int keep_builds = 20
     int keep_days = 10
